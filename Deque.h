de733398941ac5d2da8fe96efe8f0710600fe657
Angelin Lalev <lalev@uni-svishtov.bg>
2022-09-14 10:47:36 +0300

    Working on Deques

---
+++ Deque.h
@@ -0,0 +1,127 @@
+//
+// Created by lalev on 13.09.22.
+//
+
+#ifndef SIMPLIFIED_DATA_STRUCTURES_DEQUE_H
+#define SIMPLIFIED_DATA_STRUCTURES_DEQUE_H
+
+#include <memory>
+#include <optional>
+
+#define Pointer std::shared_ptr<DequeElement<T>>
+#define newDequeElement(X) (std::make_shared<DequeElement<T>>(X))
+
+using namespace std;
+
+template <class T> class DequeElement {
+public:
+    T value;
+    Pointer previous;
+    Pointer next;
+    explicit DequeElement(T newValue);
+};
+
+template <class T> class Deque {
+private:
+    Pointer first;
+    Pointer last;
+public:
+    Deque();
+    void pushFirst(T value);
+    optional<T> popFirst();
+    optional<T> peekFirst();
+    void pushLast(T value);
+    optional<T> popLast();
+    optional<T> peekLast();
+    void forEach(void (*consumer)(T value));
+};
+
+template<class T>
+DequeElement<T>::DequeElement(T newValue) : value(newValue) {}
+
+
+template<class T>
+Deque<T>::Deque() : first(nullptr), last(nullptr) {}
+
+template<class T>
+void Deque<T>::pushFirst(T value) {
+    Pointer tmp = newDequeElement(value);
+
+    if (first == nullptr) {
+        first = tmp;
+        last = tmp;
+    } else {
+        first->previous = tmp;
+        tmp->next = first;
+        first = tmp;
+    }
+}
+
+template<class T>
+optional<T> Deque<T>::popFirst() {
+    if (first==nullptr) {
+        return {};
+    }
+
+    Pointer tmp = first;
+    first = first->next;
+    if (first==nullptr) {
+        last=nullptr;
+    } else {
+        first->previous = nullptr;
+    }
+
+    return tmp->value;
+}
+
+template<class T>
+optional<T> Deque<T>::peekFirst() {
+    if (first==nullptr) return {};
+    return first->value;
+}
+
+template<class T>
+void Deque<T>::pushLast(T value) {
+    Pointer tmp = newDequeElement(value);
+
+    last->next = tmp;
+    tmp->previous = last;
+    last = tmp;
+}
+
+template<class T>
+optional<T> Deque<T>::popLast() {
+    if (last==nullptr) {
+        return {};
+    }
+
+    Pointer tmp = last;
+    last = last->previous;
+    if (last==nullptr) {
+        first=nullptr;
+    } else {
+        last->next = nullptr;
+    }
+
+    return tmp->value;
+}
+
+template<class T>
+optional<T> Deque<T>::peekLast() {
+    if (last==nullptr) return {};
+    return last->value;
+}
+
+template<class T>
+void Deque<T>::forEach(void (*consumer)(T)) {
+    Pointer tmp = first;
+    while (tmp != nullptr) {
+        consumer(tmp->value);
+        tmp = tmp->next;
+    }
+}
+
+#undef Pointer
+#undef newDequeElement
+
+#endif //SIMPLIFIED_DATA_STRUCTURES_DEQUE_H
---
+++ IntDeque.h
@@ -0,0 +1,117 @@
+//
+// Created by lalev on 14.09.22.
+//
+
+#ifndef SIMPLIFIED_DATA_STRUCTURES_INTDEQUE_H
+#define SIMPLIFIED_DATA_STRUCTURES_INTDEQUE_H
+
+#include <memory>
+#include <optional>
+
+#define Pointer std::shared_ptr<IntDequeElement>
+#define newIntDequeElement(X) (std::make_shared<IntDequeElement>(X))
+
+using namespace std;
+
+class IntDequeElement {
+public:
+    int value;
+    Pointer previous;
+    Pointer next;
+    explicit IntDequeElement(int newValue);
+};
+
+class IntDeque {
+private:
+    Pointer first;
+    Pointer last;
+public:
+    IntDeque();
+    void pushFirst(int value);
+    optional<int> popFirst();
+    optional<int> peekFirst();
+    void pushLast(int value);
+    optional<int> popLast();
+    optional<int> peekLast();
+    void forEach(void (*consumer)(int value));
+};
+
+IntDequeElement::IntDequeElement(int newValue) : value(newValue) {}
+
+IntDeque::IntDeque() : first(nullptr), last(nullptr) {}
+
+void IntDeque::pushFirst(int value) {
+    Pointer tmp = newIntDequeElement(value);
+
+    if (first == nullptr) {
+        first = tmp;
+        last = tmp;
+    } else {
+        first->previous = tmp;
+        tmp->next = first;
+        first = tmp;
+    }
+}
+
+optional<int> IntDeque::popFirst() {
+    if (first==nullptr) {
+        return {};
+    }
+
+    Pointer tmp = first;
+    first = first->next;
+    if (first==nullptr) {
+        last=nullptr;
+    } else {
+        first->previous = nullptr;
+    }
+
+    return tmp->value;
+}
+
+optional<int> IntDeque::peekFirst() {
+    if (first==nullptr) return {};
+    return first->value;
+}
+
+void IntDeque::pushLast(int value) {
+    Pointer tmp = newIntDequeElement(value);
+
+    last->next = tmp;
+    tmp->previous = last;
+    last = tmp;
+}
+
+optional<int> IntDeque::popLast() {
+    if (last==nullptr) {
+        return {};
+    }
+
+    Pointer tmp = last;
+    last = last->previous;
+    if (last==nullptr) {
+        first=nullptr;
+    } else {
+        last->next = nullptr;
+    }
+
+    return tmp->value;
+}
+
+optional<int> IntDeque::peekLast() {
+    if (last==nullptr) return {};
+    return last->value;
+}
+
+void IntDeque::forEach(void (*consumer)(int value)) {
+    Pointer tmp = first;
+    while (tmp != nullptr) {
+        consumer(tmp->value);
+        tmp = tmp->next;
+    }
+}
+
+#undef Pointer
+#undef newIntDequeElement
+
+#endif //SIMPLIFIED_DATA_STRUCTURES_INTDEQUE_H
